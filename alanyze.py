69d27fc492203ab04c45acbf35e1734cfcf84ff5
YitaoYuan <yuanyitao@pku.edu.cn>
2025-09-04 16:54:48 +0800

    add output in alanyze

--- alanyze.py
+++ alanyze.py
@@ -1,6 +1,7 @@
 import json
 import sys
 from tqdm import tqdm
+import numpy as np
 
 files = ['sg_90k_part1.json', 'sg_90k_part2.json']
 files = [f'processed_{file_name}' for file_name in files]
@@ -11,6 +12,7 @@ round_prompt_len = []
 round_new_prompt_len = []
 round_response_len = []
 seq_len_in_decode = []
+sender_set = set()
 
 for file_name in files:
     print(f"Processing file {file_name}", file=sys.stderr)
@@ -26,7 +28,7 @@ for file_name in files:
         for round_text in conversation_rounds:
             sender = round_text["from"]
             len_tokens = round_text["value"]
-            
+            sender_set.add(sender)
             if sender in ["user", "human"]:
                 new_prompt_len = len_tokens
                 round_new_prompt_len.append(new_prompt_len)
@@ -49,5 +51,13 @@ output_dict = {
     "round_response_len": round_response_len,
     "seq_len_in_decode": seq_len_in_decode,
 }
+
+print(f"sender_set: {sender_set}")
+print(f"conversations: {len(conversation_len)}")
+print(f"requests: {len(seq_len_in_decode)}")
+print(f"Mean aggregated prompt length: {np.mean([prompt_len for prompt_len, total_len, new_prompt_len in seq_len_in_decode])}")
+print(f"Mean aggregated output length: {np.mean([total_len-prompt_len for prompt_len, total_len, new_prompt_len in seq_len_in_decode])}")
+print(f"Max total length: {max([total_len for prompt_len, total_len, new_prompt_len in seq_len_in_decode])}")
+
 with open(f'figure.json', 'w', encoding='utf-8') as output_file:
     json.dump(output_dict, output_file, indent=4)
